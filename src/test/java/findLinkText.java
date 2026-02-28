0913821b2b09ff6f16be8299759748f7b216e692
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-02-28 05:04:15 +0200

    Use TagName Locator

--- src/test/java/findLinkText.java
+++ src/test/java/findLinkText.java
@@ -8,14 +8,14 @@ public class findLinkText {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        
-        driver.findElement(By.partialLinkText("JavaScript")).click();
-        Thread.sleep(1000);
-        driver.navigate().back();
+//        driver.findElement(By.partialLinkText("JavaScript")).click();
+//        Thread.sleep(1000);
+//        driver.navigate().back();
         driver.findElement(By.linkText("Form Authentication")).click();
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.className("radius")).click();
+//        driver.findElement(By.className("radius")).click();
+        driver.findElement(By.tagName("button")).click();
 
         Thread.sleep(2000);
         driver.quit();
